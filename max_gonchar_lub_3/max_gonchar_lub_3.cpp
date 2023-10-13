3fef95d51dfdc9463e7c66f1b019072c0be532f3
UserASUS <UserASUS@COMPASUS>
2023-10-13 16:51:36 +0300

    Добавьте файлы проекта.

---
+++ max_gonchar_lub_3/max_gonchar_lub_3.cpp
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+#include<locale.h>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	float in, m, mi;
+	printf("Введите дюимы: ");
+	scanf_s("%f", &in);
+	printf("\n");
+	m = in * 0.025;
+	mi = m * 1.41;
+	printf("Аршины: %2f\n", mi);
+	printf("\n");
+	printf("Повторить? Если нет введите n: ");
+}
